76d6d5b6b39eeb31379cde0a9401b0d7ce5e0e9f
Kitpipat Jaritwong <Kitpipat.0647@gmail.com>
2021-11-13 21:36:42 +0700

    oof2

--- pages/log.vue
+++ pages/log.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex h-screen">
+  <div class="flex flex-col h-screen">
     <div class="mx-auto">
       <div class="my-8">
         <h1 class="text-4xl text-center font-500 certTitlefadein">
@@ -7,7 +7,7 @@
         </h1>
       </div>
     </div>
-    <div class="grid grid-cols-1 grid-flow-row gap-y-12 mt-16">
+    <div class="flex flex-col space-y-6 mt-16">
       <p>[11/13/2021 - 21:00:53]  [+] Socail & Science subject</p>
       <p>[11/13/2021 - 21:27:41]  [+] Logs page</p>
       <p>[11/13/2021 - 21:27:41]  [=] fix nav logging for mobile</p>
